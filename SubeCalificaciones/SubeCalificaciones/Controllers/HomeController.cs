c545e67d4e2d30b50a449547e782dc276c0b9f05
Fede <fede._perez@hotmail.com>
2019-06-18 00:09:01 -0300

    Updates for login page

--- SubeCalificaciones/SubeCalificaciones/Controllers/HomeController.cs
+++ SubeCalificaciones/SubeCalificaciones/Controllers/HomeController.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SubeCalificaciones.Models;
 
 namespace SubeCalificaciones.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Ingresar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ingresar(UsuarioAnonimo ua)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ua.IsProfesor == true)
+                {
+                    return RedirectToAction("Index","Profesor");
+                } else
+                {
+                    return RedirectToAction("Index", "Alumno");
+                }
+            }
+            else
+            {
+                return View(ua);
+            }
+
         }
 
     }
---
+++ SubeCalificaciones/SubeCalificaciones/Models/UsuarioAnonimo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SubeCalificaciones.Models
+{
+    public class UsuarioAnonimo
+    {
+        [Required(ErrorMessage = "El email es requerido.")]
+        public string InputEmail { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "La contraseña es requerida.")]
+        public string InputPassword { get; set; }
+
+        public bool IsProfesor { get; set; }
+    }
+}
